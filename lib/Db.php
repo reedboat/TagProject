038a9d1d2e87c637f09f99c5a00b12e59ffb224e
zhangqiuming <zhqm03@gmail.com>
2012-08-29 18:02:09 -0700

    连接建立参数修改

--- lib/Db.php
+++ lib/Db.php
@@ -1,8 +1,33 @@
 <?php
 class Db {
     private $db = null;
+    private $adapter = '';
 
     private static $instance;
+    
+    public function __construct($db_config=null){
+        if (is_array($db_config) && (isset($db_config['host']) || isset($db_config['dsn']))){
+            if (!isset($db_config['dsn'])) {
+                if(!isset($db_config['adapter'])){
+                    $this->adapter = 'mysql';
+                }
+                $dsn = $this->adapter . ":host=" . $db_config['host'] . ";port=" . $db_config['port'];
+                if (isset($db_config['dbname']){
+                    $dsn .= ";dbname=" . $db_config['dbname'];
+                }
+                $options = array();
+                if ($this->adapter == 'mysql') {
+                    $options = array(
+                        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES ' . $db_config['enCode'],
+                    );
+                }
+                $this->connect($dsn, $db_config['username'], $db_config['password'], $options);
+            }
+            else {
+                $this->connect($db_config['dsn'], $db_config['username'], $db_config['password'], array());
+            }
+        }
+    }
     
     public static function instance(){
         if (self::$instance == null){
@@ -13,14 +38,10 @@ class Db {
     }
 
     public function getDBConnection(){
-        return self::$db;
+        return $this->db;
     }
     public function setDBConnection($db){
-        self::$db = $db;
-    }
-
-    public static function makeConnection($dsn, $username='', $password='', $driver_options=array()){
-        return new PDO($dsn, $username, $password, $driver_options);
+        $this->db = $db;
     }
 
     public function selectDB($dbname){
